796b7bf31c8bb5f3dc9c9a7cef76e399bd1e5e99
Herni <h.britosbogado@gmail.com>
2020-09-02 20:51:53 -0300

    ok

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,3 +1,4 @@
+  
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
@@ -8,5 +9,26 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    idProduct: {
+      type: DataTypes.INTEGER,
+      primaryKey:true,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.REAL,
+    },
+    img: {
+      type: DataTypes.TEXT,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+    },
+    category: {
+      type: DataTypes.STRING,
+    }
+
+ });
+
 };
