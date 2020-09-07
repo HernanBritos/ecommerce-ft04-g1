053b257f9b1882353a3f2a049bfa9d114e71d165
Herni <h.britosbogado@gmail.com>
2020-09-07 15:58:25 -0300

    asdsd

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -9,7 +9,6 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    
     description: {
       type: DataTypes.STRING,
     },
--- client/src/container/formProducto.js
+++ client/src/container/formProducto.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import cComponent from "./css/formproducto.module.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { AdminAddCategory } from "./adminAddCategory.js";
 
 export default function FormProduct() {
   const [input, setInput] = useState({
@@ -12,6 +13,8 @@ export default function FormProduct() {
     stock: "",
     img: "",
   });
+
+  const [categories, setCategories] = useState([]);
 
   const [errors, setErrors] = React.useState({});
 
@@ -26,6 +29,19 @@ export default function FormProduct() {
     //     [e.target.name]: e.target.value,
     //   })
     // );
+  };
+
+
+
+  const handleCategoryInputChange = function (e) {
+
+    var categories = [];
+    categories.push(e.target.id)
+    setInput({
+      ...input,
+      nameCat: categories,
+    });
+    console.log(categories);
   };
 
   const handleSubmit = function (e) {
@@ -44,6 +60,12 @@ export default function FormProduct() {
       });
     return (window.location = "http://localhost:3000/admin");
   };
+
+ useEffect(() => {
+    axios.get("http://localhost:3001/categories/").then((response) => {
+        setCategories(response.data);
+    });
+  },categories)
 
   return (
     <div className={cComponent.back}>
@@ -66,14 +88,34 @@ export default function FormProduct() {
           </div>
           <div className="form-group">
             <label htmlFor="productname">Categoria: </label>
-            <input
+            {/* <input
               name="category"
               value={input.category}
               type="text"
               onChange={handleInputChange}
               className="form-control"
               id="ProductCategory"
-            />
+            /> */}
+             {/* 
+    constructor() {
+    super();
+    this.state = { categories: [] };
+       }
+        */}
+        <button id="selectedDBXP" aria-expanded="false" aria-labelledby="lblDBXP DBXPList" onclick="return DBXPclick();">
+          <span id="DBXPList"></span>
+            <i class="fa fa-chevron-down" aria-hidden="true"></i>
+            </button>
+            <fieldset aria-labelledby="lblDBXP">
+              <div id="choicelist">
+              {categories && categories.map(cat => (
+                <div>
+                  <input name="DBXP" type="checkbox" id={cat.id} onChange={handleCategoryInputChange} />
+                  <label for={cat.id} aria-label="categories">{cat.name}</label>
+                </div>
+              ))}
+              </div>
+            </fieldset>
           </div>
           <div className="form-group">
             <label htmlFor="description">Descripcion: </label>
