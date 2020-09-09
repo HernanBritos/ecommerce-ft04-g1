c3f4001e906fef21b2ad91f012199e7f9c486eab
valenfontana7 <valenfontana7@gmail.com>
2020-09-09 15:53:57 -0300

    cambios1

--- client/src/container/catalogComponent.js
+++ client/src/container/catalogComponent.js
@@ -1,28 +1,34 @@
 import React from "react";
 import cComponent from "./css/catalogComponent.module.css";
 import ProductCard from "../components/productCard";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-function CatalogComponent (props) {
+function CatalogComponent(props) {
   const [products, setProducts] = useState([]);
-  useEffect(()=> {
+  useEffect(() => {
     axios.get("http://localhost:3001/products").then((data) => {
-      setProducts( data.data );
+      setProducts(data.data);
     });
-  },products) 
-      return (
-        <div className={cComponent.catalog}>
-          {products && products.map((product) => {
-            return (
-              <div key={product.id} className={cComponent.pCard}>
-                <div>
-                  <ProductCard producto={product} />
-                </div>
+  }, products);
+  return (
+    <div className={cComponent.catalog}>
+      {products.length !== 0 ? (
+        products.map((product) => {
+          return (
+            <div key={product.id} className={cComponent.pCard}>
+              <div>
+                <ProductCard producto={product} />
               </div>
-            );
-          })}
+            </div>
+          );
+        })
+      ) : (
+        <div className={` ${cComponent.alerta} alert alert-warning`}>
+          Oops! No hay productos creados aún. Prueba creando uno.
         </div>
-      );
+      )}
+    </div>
+  );
 }
 export default CatalogComponent;
--- client/src/container/productCategory.js
+++ client/src/container/productCategory.js
@@ -1,51 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import cComponent from "./css/catalogComponent.module.css";
 import ProductCard from "../components/productCard";
 
+function ProductCategory(props) {
+  const [products, setProducts] = useState([]);
 
-class ProductCategory extends React.Component {
- 
-constructor(props) {
-    super(props);
-    this.props = props;
-    this.state = { products: [] };
-    
-}
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/products/categoria/${props.nombrecat}`)
+      .then((data) => {
+        setProducts(data.data);
+        console.log(props.nombrecat);
+      });
+  }, products);
 
-  componentDidMount() {
-    axios.get(`http://localhost:3001/products/categoria/${this.props.nombrecat}`).then((data) => {
-      this.setState({ products: data.data });
-     console.log(this.props.nombrecat)
-   });
-  } 
-
-  render() {
-   
-    return (
-      <div>
-         <Link to="/">
-          <button className="btn btn-primary">
-            Pagina de Inicio
-          </button>
-          </Link>
-      
-        <div className={cComponent.catalog}>
-        {this.state.products.map((product) => {
-            return (
-              <div key={product.id} className={cComponent.pCard}>
-                <div>
-                  <ProductCard producto={product} />
-                </div>
-                
+  return (
+    <div>
+      <Link to="/">
+        <button className="btn btn-primary">Pagina de Inicio</button>
+      </Link>
+      <div className={cComponent.catalog}>
+        {products.map((product) => {
+          return (
+            <div key={product.id} className={cComponent.pCard}>
+              <div>
+                <ProductCard producto={product} />
               </div>
-            );
-          })}
-        </div>
-        </div>
-      );
-    } 
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default ProductCategory;
